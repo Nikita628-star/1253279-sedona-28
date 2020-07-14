d73ab40050a5ae37a1be56015de247ce973c2524
Nikita_Genkin <toliapetrovskiy@gmail.com>
2020-07-14 23:23:04 +0300

    Finished work number 2

--- js/script.js
+++ js/script.js
@@ -1,16 +1,2 @@
-var button = document.querySelector(".search");
-var popup = document.querySelector(".hotel-search");
-
-button.addEventListener("click", function (evt) {
-  evt.preventDefault();
-  var classes = popup.className.split(' ');
-
-  if (classes.length > 1) {
-    popup.classList.remove("hotel-search-show");
-  }
-
-  else {
-    popup.classList.add("hotel-search-show");
-  }
-});
+var button=document.querySelector(".search");var popup=document.querySelector(".hotel-search");button.addEventListener("click",function(a){a.preventDefault();var b=popup.className.split(" ");if(b.length==1){popup.classList.add("hotel-search-remove")}else{popup.classList.remove("hotel-search-remove")}});
 
